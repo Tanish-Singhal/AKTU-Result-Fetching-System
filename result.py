d19f60f5a4712794ece04b1b773c1927a6931614
Tanish Singhal <tanishsinghal510@gmail.com>
2023-11-13 17:59:06 +0530

    comments added

--- result.py
+++ result.py
@@ -11,24 +11,28 @@ df = pd.read_excel(r'C:\\Users\\Tanish Singhal\\Desktop\\AKTU result Mini Projec
 driver = webdriver.Chrome()
 driver.get('https://erp.aktu.ac.in/webpages/oneview/oneview.aspx')
 
+# entering the roll no from the excel file
 wait = WebDriverWait(driver, 10)
 roll_number_input = wait.until(EC.presence_of_element_located((By.ID, 'txtRollNo')))
 time.sleep(3)
-
 roll_number_input.send_keys(str(df['rollno'].iloc[0]))
 
+# click on proceed button
 submit_button = wait.until(EC.element_to_be_clickable((By.ID, 'btnProceed')))
 submit_button.click()
 
+# entering the date of birth from the excel file
 dob_input = wait.until(EC.presence_of_element_located((By.ID, 'txtDOB')))
 time.sleep(4)
 
 formatted_dob = df['dob'].iloc[0].strftime('%Y-%m-%d')
 dob_input.send_keys(formatted_dob)
 
+# click on proceed button
 submit_button = wait.until(EC.element_to_be_clickable((By.ID, 'btnProceed')))
 submit_button.click()
 
+# Captcha Part
 captcha_checkbox = wait.until(EC.presence_of_element_located((By.CLASS_NAME, 'recaptcha-checkbox-border')))
 
 specific_part_location = captcha_checkbox.location
