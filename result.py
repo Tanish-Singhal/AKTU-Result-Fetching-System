6a704dd8da94aa0d0c86e682ef779b3f016b3263
Tanish Singhal <tanishsinghal510@gmail.com>
2023-11-17 12:09:22 +0530

    except the captcha, whole coding part done

--- result.py
+++ result.py
@@ -2,51 +2,51 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
-from selenium.webdriver.common.action_chains import ActionChains
 import pandas as pd
-import time
 
 df = pd.read_excel(r'C:\\Users\\Tanish Singhal\\Desktop\\AKTU result Mini Project.xlsx')
 
 driver = webdriver.Chrome()
-driver.get('https://erp.aktu.ac.in/webpages/oneview/oneview.aspx')
+initial_url = 'https://erp.aktu.ac.in/webpages/oneview/oneview.aspx'
+driver.get(initial_url)
 
-# TODO: entering the roll no from the excel file
-wait = WebDriverWait(driver, 10)
-roll_number_input = wait.until(EC.presence_of_element_located((By.ID, 'txtRollNo')))
-time.sleep(3)
-roll_number_input.send_keys(str(df['rollno'].iloc[0]))
+wait = WebDriverWait(driver, 5)
 
-# TODO: click on proceed button
-submit_button = wait.until(EC.element_to_be_clickable((By.ID, 'btnProceed')))
-submit_button.click()
+for index, row in df.iterrows():
+    # TODO: Roll No part
+    roll_number_input = wait.until(EC.presence_of_element_located((By.ID, 'txtRollNo')))
+    roll_number_input.clear()
+    roll_number_input.send_keys(str(row['rollno']))
 
-# TODO: entering the date of birth from the excel file
-dob_input = wait.until(EC.presence_of_element_located((By.ID, 'txtDOB')))
-time.sleep(4)
+    submit_button = wait.until(EC.element_to_be_clickable((By.ID, 'btnProceed')))
+    submit_button.click()
 
-formatted_dob = df['dob'].iloc[0].strftime('%Y-%m-%d')
-dob_input.send_keys(formatted_dob)
+    # TODO: Date of Birth Part
+    dob_input = wait.until(EC.presence_of_element_located((By.ID, 'txtDOB')))
+    formatted_dob = row['dob'].strftime('%Y-%m-%d')
+    dob_input.clear()
+    dob_input.send_keys(formatted_dob)
 
-# TODO: Captcha Part (ongoing)
-# Wait for the reCAPTCHA iframe to be present
-captcha_frame = WebDriverWait(driver, 10).until(
-    EC.presence_of_element_located((By.XPATH, ".//iframe[@title='reCAPTCHA']"))
-)
+    # TODO: Captcha Manually Solve
+    captcha_input = input("Please solve the captcha manually and press Enter when done: ")
 
-driver.switch_to.frame(captcha_frame)
+    submit_button = wait.until(EC.element_to_be_clickable((By.ID, 'btnSearch')))
+    submit_button.click()
 
-captcha_checkbox = WebDriverWait(driver, 10).until(
-    EC.element_to_be_clickable((By.ID, 'recaptcha-anchor-label'))
-)
+    # FIXME: Second page start
+    # Move to the targeted div
+    second_page_elements = wait.until(EC.presence_of_all_elements_located((By.XPATH, '//tbody/tr[2]/td/div')))
+    
+    # Extract the data from the last div and update it
+    last_div_data = second_page_elements[-1].text
+    df.loc[index, 'Result'] = last_div_data
 
-captcha_checkbox.click()
+    # save the data in the excel file
+    df.to_excel(r'C:\\Users\\Tanish Singhal\\Desktop\\AKTU result Mini Project.xlsx', index=False)
 
-driver.switch_to.default_content()
+    print(f"Data for Roll No {row['rollno']} from the last div in the second page:", last_div_data)
 
-# TODO: click on proceed button
-submit_button = wait.until(EC.element_to_be_clickable((By.ID, 'btnProceed')))
-submit_button.click()
-
+    # Run the main URL again, to fetch the data of the next student
+    driver.get(initial_url)
 
 driver.quit()
