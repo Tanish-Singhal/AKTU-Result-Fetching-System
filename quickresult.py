f8898f0f603e21728102136e3f049326ef47b647
Tanish Singhal <tanishsinghal510@gmail.com>
2023-12-12 23:55:20 +0530

    some modification in the quickresult code

--- quickresult.py
+++ quickresult.py
@@ -5,7 +5,7 @@ from selenium.webdriver.support import expected_conditions as EC
 import pandas as pd
 import time
 
-# TODO: Load the Excel file and the website
+# Load the Excel file and the website
 df = pd.read_excel(r'C:\\Users\\Tanish Singhal\\Desktop\\AKTU result Mini Project.xlsx')
 
 driver = webdriver.Chrome()
@@ -15,7 +15,7 @@ driver.get(initial_url)
 wait = WebDriverWait(driver, 5)
 
 for index, row in df.iterrows():
-    # TODO: Roll No part
+    # Roll No part
     roll_number_input = wait.until(EC.presence_of_element_located((By.ID, 'txtRollNo')))
     roll_number_input.clear()
     roll_number_input.send_keys(str(row['rollno']))
@@ -23,28 +23,53 @@ for index, row in df.iterrows():
     submit_button = wait.until(EC.element_to_be_clickable((By.ID, 'btnProceed')))
     submit_button.click()
 
-    # TODO: Date of Birth Part
+    # Date of Birth Part
     dob_input = wait.until(EC.presence_of_element_located((By.ID, 'txtDOB')))
     formatted_dob = row['dob'].strftime('%Y-%m-%d')
     dob_input.clear()
     dob_input.send_keys(formatted_dob)
 
-    # TODO: Captcha Manually Solve
+    # Captcha Manually Solve
     captcha_input = input("Please solve the captcha manually and press Enter when done: ")
 
     submit_button = wait.until(EC.element_to_be_clickable((By.ID, 'btnSearch')))
     submit_button.click()
 
-    # TODO: Second page start
+    # Second page start
     # TODO: Main div data extracted
     second_page_elements = wait.until(EC.presence_of_all_elements_located((By.XPATH, '//tbody/tr[2]/td/div')))
 
-    # Create new columns for each div
-    for i, div_element in enumerate(second_page_elements, start=1):
+    # Initialize dictionaries for each div
+    div_data_dicts = []
+
+    # Extract data for each div
+    for div_element in second_page_elements:
         div_data = div_element.text
-        column_name = f'div_{i}'  # Create a unique column name for each div
-        df.loc[index, column_name] = div_data
- 
+
+        # Check if the expected data exists before trying to access it
+        session = div_data.split('Session : ')[1].split(' ')[0] if 'Session' in div_data else ''
+        semesters = div_data.split('Semesters : ')[1].split(' ')[0] if 'Semesters' in div_data else ''
+        result = div_data.split('Result : ')[1].split(' ')[0] if 'Result' in div_data else ''
+        marks = div_data.split('Marks : ')[1].split(' ')[0] if 'Marks' in div_data else ''
+        cop = div_data.split('COP : ')[1].split(' ')[0] if 'COP' in div_data else ''
+
+        div_dict = {
+            'Session': session,
+            'Semesters': semesters,
+            'Result': result,
+            'Marks': marks,
+            'COP': cop
+        }
+        div_data_dicts.append(div_dict)
+
+    # Update DataFrame with div data
+    for i, div_dict in enumerate(div_data_dicts, start=1):
+        df.loc[index, f'Session_div{i}'] = div_dict.get('Session', '')
+        df.loc[index, f'Semesters_div{i}'] = div_dict.get('Semesters', '')
+        df.loc[index, f'Result_div{i}'] = div_dict.get('Result', '')
+        df.loc[index, f'Marks_div{i}'] = div_dict.get('Marks', '')
+        df.loc[index, f'COP_div{i}'] = div_dict.get('COP', '')
+
     # TODO: Save the data in the Excel file
     df.to_excel(r'C:\Users\Tanish Singhal\Desktop\AKTU result Mini Project.xlsx', index=False)
 
